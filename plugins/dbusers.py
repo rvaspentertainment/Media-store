2fd6728e923b61b617f53fd434a77d4cfbba86d9
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2024-12-18 20:46:00 +0530

    Update dbusers.py

--- plugins/dbusers.py
+++ plugins/dbusers.py
@@ -13,6 +13,7 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
         self.grp = self.db.groups
+        self.user_data = self.db.user_data
 
 
     def new_user(self, id, name):
