f4df80d2c54b4120ddeb94ecf9814d2f47fd73bb
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2025-01-03 19:45:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -177,7 +177,7 @@ async def start(client, message):
 
         data = message.command[1]
         try:
-            pre, file_id, movies_no = data.split('_', 1)
+            pre, file_id, movies_no = data.text.split(" ", 1)  # Split the message into command and argument
         except ValueError:
             file_id = data
             pre = ""
